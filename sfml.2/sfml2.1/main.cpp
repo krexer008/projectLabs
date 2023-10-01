d33361690d8930b6e325ec56e4148023c8af6471
krexer008 <y.fedor86@mail.ru>
2023-10-01 19:34:02 +0300

    Wave Moveng Ball and Wall

--- sfml.2/sfml2.1/main.cpp
+++ sfml.2/sfml2.1/main.cpp
@@ -10,12 +10,14 @@ int main()
 {
     constexpr float BALL_SIZE = 40;
 
-    sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Wave Moveng Ball");
+    sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Wave Moveng Ball and Wall");
     sf::Clock clock;
 
-    const sf::Vector2f position = {10, 350};
+    sf::Vector2f position = {10, 350};
+
     constexpr float speedX = 100.f;
     float speedXE = speedX;
+    float elapsedTime = 0;
     constexpr float amplitudeY = 80.f;
     constexpr float periodY = 2;
 
@@ -33,22 +35,29 @@ int main()
             }
         }
 
-        if ((position.x + 2 * BALL_SIZE >= WINDOW_WIDTH) && (speedXE > 0))
+        if ((position.x + 2 * BALL_SIZE >= WINDOW_WIDTH) && (speedXE > 0) || (position.x < 0) && (speedXE < 0))
         {
             speedXE = -speedXE;
         }
+        /*
         if ((position.x < 0) && (speedXE < 0))
         {
             speedXE = -speedXE;
         }
+        */
 
-        const float time = clock.getElapsedTime().asSeconds();
-        const float wavePhase = time * float(2 * M_PI);
-        const float x = speedX * time;
+        const float deltaTime = clock.restart().asSeconds();
+
+        // y move
+        elapsedTime += deltaTime;
+        const float wavePhase = elapsedTime * float(2 * M_PI);
         const float y = amplitudeY * std::sin(wavePhase / periodY);
-        const sf::Vector2f offset = {x, y};
 
-        ball.setPosition(position + offset);
+        float x = speedXE * deltaTime;
+        position.x += x;
+        const sf::Vector2f offset = {position.x, position.y + y};
+
+        ball.setPosition(offset);
 
         //Рисование текущего состояния
         window.clear();
